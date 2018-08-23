08f3fbab63ecf4de0c6a9cf4ed13ec8df238b9b3
Michael Haileyesus <ayele@cs.helsinki.fi>
2018-08-23 11:45:38 +0300

    pagination component is stated to build

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Movies from "./components/movies";
+import Pagination from "./components/common/pagination";
 
 class App extends Component {
   render() {
     return (
       <main role="main" className="container">
         <Movies />
+        <Pagination />
       </main>
     );
   }
---
+++ src/components/common/pagination.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+const Pagination = () => {
+  return (
+    <nav aria-label="Page navigation example">
+      <ul className="pagination">
+        <li className="page-item">
+          <a className="page-link" href="#">
+            1
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
