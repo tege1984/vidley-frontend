3a951f384271f8162f9a265d5d8e2a6915a906dc
Michael Haileyesus <ayele@cs.helsinki.fi>
2018-08-29 15:34:13 +0300

    TableBody component is added

--- src/components/common/TableBody.jsx
+++ src/components/common/TableBody.jsx
@@ -1,10 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Like from "./Like";
 
 class TableBody extends Component {
-    
-    render() { 
-        return (  );
-    }
+  render() {
+    return (
+      <tbody>
+        {this.props.movies.map(m => (
+          <tr key={m._id}>
+            <td> {m.title} </td>
+            <td>{m.genre.name}</td>
+            <td>{m.numberInStock}</td>
+            <td>{m.dailyRentalRate}</td>
+            <td>
+              <Like liked={m.liked} onClick={() => this.props.onLike(m)} />
+            </td>
+            <td>
+              <button
+                onClick={() => this.props.onDelete(m)}
+                className="btn btn-danger btn-sm m-2"
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    );
+  }
 }
- 
+
 export default TableBody;
--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import Like from "./common/Like";
 import TableHeader from "./common/TableHeader";
+import TableBody from "./common/TableBody";
 
 class MoviesTable extends Component {
   render() {
@@ -21,29 +21,7 @@ class MoviesTable extends Component {
           sortColumn={sortColumn}
           columns={columns}
         />
-
-        <tbody>
-          {movies.map(movie => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                <Like liked={movie.liked} onClick={() => onLike(movie)} />
-              </td>
-              <td>
-                <button
-                  onClick={() => onDelete(movie)}
-                  className="btn btn-danger btn-sm m-2"
-                >
-                  {" "}
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
+        <TableBody movies={movies} onLike={onLike} onDelete={onDelete} />
       </table>
     );
   }
