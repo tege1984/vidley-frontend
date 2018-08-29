e6d6be4ad0e9f974ccfa10a45cfbe40986e8389b
Michael Haileyesus <ayele@cs.helsinki.fi>
2018-08-29 15:13:18 +0300

    TableHeader components is added

---
+++ src/components/common/TableHeader.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class TableHeader extends Component {
+  raiseSort = path => {
+    const sortColumn = { ...this.props.sortColumn };
+    if (sortColumn.path === path)
+      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
+    else {
+      sortColumn.path = path;
+      sortColumn.order = "asc";
+    }
+    this.props.onSort(sortColumn);
+  };
+  render() {
+    return (
+      <thead>
+        <tr>
+          {this.props.columns.map(c => (
+            <th
+              key={c.path || c.key}
+              style={{ cursor: "pointer" }}
+              onClick={() => this.raiseSort(c.path)}
+            >
+              {c.lable}
+            </th>
+          ))}
+        </tr>
+      </thead>
+    );
+  }
+}
+
+export default TableHeader;
--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,52 +1,27 @@
 import React, { Component } from "react";
 import Like from "./common/Like";
+import TableHeader from "./common/TableHeader";
 
 class MoviesTable extends Component {
-  raiseSort = path => {
-    const sortColumn = { ...this.props.sortColumn };
-    if (sortColumn.path === path)
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
-    this.props.onSort(sortColumn);
-  };
-
   render() {
-    const { movies, onLike, onDelete } = this.props;
+    const columns = [
+      { path: "title", lable: "Title" },
+      { path: "genre.name", lable: "Genre" },
+      { path: "numberInStock", lable: "Stock" },
+      { path: "dailyRentalRate", lable: "Rate" },
+      { key: "like" },
+      { key: "delete" }
+    ];
+
+    const { movies, onLike, onDelete, onSort, sortColumn } = this.props;
     return (
       <table className="table">
-        <thead>
-          <tr>
-            <th
-              style={{ cursor: "pointer" }}
-              onClick={() => this.raiseSort("title")}
-            >
-              Title
-            </th>
-            <th
-              style={{ cursor: "pointer" }}
-              onClick={() => this.raiseSort("genre.name")}
-            >
-              Gener
-            </th>
-            <th
-              style={{ cursor: "pointer" }}
-              onClick={() => this.raiseSort("numberInStock")}
-            >
-              Stoke
-            </th>
-            <th
-              style={{ cursor: "pointer" }}
-              onClick={() => this.raiseSort("dailyRentalRate")}
-            >
-              Rate
-            </th>
-            <th />
-            <th />
-          </tr>
-        </thead>
+        <TableHeader
+          onSort={onSort}
+          sortColumn={sortColumn}
+          columns={columns}
+        />
+
         <tbody>
           {movies.map(movie => (
             <tr key={movie._id}>
@@ -55,8 +30,7 @@ class MoviesTable extends Component {
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
               <td>
-                {" "}
-                <Like liked={movie.liked} onClick={() => onLike(movie)} />{" "}
+                <Like liked={movie.liked} onClick={() => onLike(movie)} />
               </td>
               <td>
                 <button
