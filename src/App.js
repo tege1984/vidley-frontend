9809d86836b948d6bd6b146a5810fd4cb0df2b16
Michael Haileyesus <ayele@cs.helsinki.fi>
2018-09-04 14:33:07 +0300

    notFound and moviesForm components are added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Movies from "./components/movies";
 import { NavLink, Route, Switch, Redirect } from "react-router-dom";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import MoviesForm from "./components/moviesForm";
 
 class App extends Component {
   render() {
@@ -34,10 +36,13 @@ class App extends Component {
 
         <main role="main" className="container">
           <Switch>
+            <Route path="/movies/:id" component={MoviesForm} />
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
             <Route path="/rentals" component={Rentals} />
-            <Redirect from="/" to="/movies" />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
           </Switch>
         </main>
       </React.Fragment>
---
+++ src/components/moviesForm.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+const MoviesForm = () => {
+  return <h1> movies Form</h1>;
+};
+
+export default MoviesForm;
