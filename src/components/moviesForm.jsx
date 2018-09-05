31d2f0ccf71587c49fdbe5fb0837894b42e2d438
Michael Haileyesus <ayele@cs.helsinki.fi>
2018-09-05 10:50:38 +0300

    the exercise is completed

--- src/components/moviesForm.jsx
+++ src/components/moviesForm.jsx
@@ -1,6 +1,19 @@
-import React from "react";
-const MoviesForm = () => {
-  return <h1> movies Form</h1>;
-};
+import React, { Component } from "react";
+
+class MoviesForm extends Component {
+  handleClick = () => {
+    this.props.history.push("/movies");
+  };
+  render() {
+    return (
+      <div>
+        <h1> movies Form {this.props.match.params.id} </h1>
+        <button onClick={this.handleClick} className="btn btn-primary">
+          Save
+        </button>
+      </div>
+    );
+  }
+}
 
 export default MoviesForm;
