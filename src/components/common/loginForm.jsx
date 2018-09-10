90a8bfc76c8701ad6e43d5be6bda3b288f6b86b6
Michael Haileyesus <ayele@cs.helsinki.fi>
2018-09-10 12:09:41 +0300

    10.09.2018

--- src/components/common/loginForm.jsx
+++ src/components/common/loginForm.jsx
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-  username = React.createRef();
-
+  state = {
+    account: { username: "", password: "" }
+  };
+  //username = React.createRef();
   //   componentDidMount() {
   //     this.username.current.focus();
   //   }
 
   handleSubmit = e => {
     e.preventDefault();
-    const username = this.username.current.value;
-    console.log("form submit", username);
+    // call to server
+    // const username = this.username.current.value;
+    // console.log("form submit", username);
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    //console.log(e);
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
   };
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login Form</h1>
@@ -21,15 +32,25 @@ class LoginForm extends Component {
             <label htmlFor="username">Username</label>
             <input
               autoFocus
-              ref={this.username}
+              // ref={this.username}
+              value={account.username}
+              onChange={this.handleChange}
               id="username"
+              name="username"
               type="text"
               className="form-control"
             />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              id="password"
+              name="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
